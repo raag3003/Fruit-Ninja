0fa5bffc97d2f0387fbae10b74d6a06a25e0c0c6
raag3003 <144060124+raag3003@users.noreply.github.com>
2025-06-08 14:47:26 +0200

    should be 360 now
    Created a spawn circle around the player that launches the fruits up into the air around the player

--- Fruit-ninja-Unity/Assets/Scripts/Blade.cs
+++ Fruit-ninja-Unity/Assets/Scripts/Blade.cs
@@ -55,8 +55,9 @@ public class Blade : MonoBehaviour
         if (Input.touchCount > 0)
         {
             // Vector3 position = mainCamera.ScreenToWorldPoint(Input.mousePosition); // Virker kun med musemakøren ikke fingre.
+            float sliceDepth = 2f; // tillader slice af object væk fra player
             Vector3 position = mainCamera.ScreenToWorldPoint(new Vector3(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y, mainCamera.nearClipPlane));
-            position.z = 0f;
+            //position.z = 0f;
             transform.position = position;
 
             slicing = true;
