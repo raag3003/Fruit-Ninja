0fd31fb5ea876367ba1d7457b154d4d6018b95ad
raag3003 <144060124+raag3003@users.noreply.github.com>
2025-06-08 22:23:59 +0200

    Update FruitSpawner.cs

--- Fruit-ninja-Unity/Assets/Scripts/FruitSpawner.cs
+++ Fruit-ninja-Unity/Assets/Scripts/FruitSpawner.cs
@@ -18,12 +18,20 @@ public class FruitSpawner : MonoBehaviour
 
         for (int i = 0; i < fruitCount; i++)
         {
-            float angle = 1 * Mathf.PI * 2 / fruitCount;
-            float x = Mathf.Cos(angle) * spawnRadius;
-            float z = Mathf.Sin(angle) * spawnRadius;
+            float angle = i * (360 / fruitCount); // Divide the circle
+            float radians = angle * Mathf.Deg2Rad;
+            
+            float x = Mathf.Cos(radians) * spawnRadius;
+            float z = Mathf.Sin(radians) * spawnRadius;
 
-            Vector3 spawnPos = new Vector3(groundPos.x + x, groundPos.y, groundPos.z + z);
-            Instantiate(fruitPrefab, spawnPos, Quaternion.identity);
+            float randomHeight = Random.Range(0.2f, 1.0f); // toss height of the fruit
+
+            Vector3 spawnPos = new Vector3(groundPos.x + x, groundPos.y + randomHeight, groundPos.z + z);
+            GameObject fruit = Instantiate(fruitPrefab, spawnPos, Quaternion.identity);
+
+            //Rotate outward from center
+            Vector3 tossDirection = (spawnPos - groundPos).normalized;
+            fruit.transform.rotation = Quaternion.LookRotation(tossDirection, Vector3.up);
         }
     }
 }
