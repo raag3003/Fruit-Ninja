9404f20265f69188db5e0e4fa4bf41d41ed17ecf
dast0012 <144061710+dast0012@users.noreply.github.com>
2025-06-09 14:40:25 +0200

    Permission tilføjet i starten
    Der er nu tilføjet to anmodninger i starten af spillet, som anmoder om at kunne få adgang til brugerens kamera og lokation.

--- Fruit-ninja-Unity/Assets/Scripts/MainMenu.cs
+++ Fruit-ninja-Unity/Assets/Scripts/MainMenu.cs
@@ -1,8 +1,25 @@
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.Android;
 
 public class MainMenu : MonoBehaviour
 {
+    private void Awake()
+    {
+        if (!Permission.HasUserAuthorizedPermission(Permission.Camera))
+        {
+            Permission.RequestUserPermission(Permission.Camera);
+            return;
+        }
+    }
+    private void Start()
+    {
+        if (!Permission.HasUserAuthorizedPermission(Permission.FineLocation))
+        {
+            Permission.RequestUserPermission(Permission.FineLocation);
+        }
+    }
+
     public void returnHome()
     {
         SceneManager.LoadScene(0);
